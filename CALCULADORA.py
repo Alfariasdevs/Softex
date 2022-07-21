bd65c35d126018a81d5f2cbf0d47ba2c29aba860
Alfariasdevs <74870216+Alfariasdevs@users.noreply.github.com>
2022-07-21 16:42:48 -0300

    Add files via upload

---
+++ CALCULADORA.py
@@ -0,0 +1,18 @@
+print('CALCULADORA:')
+var1 = float( input("Digite um número: ") )
+var2 = float( input("Digite outro número: ") )
+operacao = int( input("Escolha a operação a ser executada: 1.Soma, 2.Subtração, 3.Multiplicação, 4.Divisão."))
+
+if operacao == 1:
+    print(var1 + var2)
+elif operacao == 2:
+    print(var1 - var2)
+elif operacao == 3:
+    print(var1 * var2)
+elif operacao == 4:
+    if var2 == 0:
+        print("Impossível dividir por zero")
+    else:
+        print(var1 / var2)
+       
+  
